85e943271db51caac6d6025182ab898e50d6d411
jmiguelal <j.alvear@uniandes.edu.co>
2023-09-05 22:25:54 -0500

    cambios2

--- LyM.py
+++ LyM.py
@@ -13,7 +13,7 @@ def cargar_archivo()-> str:
     return codigo
 
 
-patron = r'\b\w+\b'
+patron = r'[\w]+|[(){}]'
 
 def tokenizar(patron, codigo)-> list:
     tokens = re.findall(patron, codigo)
@@ -30,33 +30,71 @@ print(tokens)
 
 
 def checkwalk (tokens):
+    direcciones = ["front", "right", "left", "back"]
+    cardinales = ["north", "south", "west", "east"]
     l = len(tokens)
-    for i in range(0, l):
+    i = 0
+    while i <l :       
         if tokens[i] == "walk":
-            try:
-                p = int(tokens[i+1])
-            except:
-                p = None
-            if p is not None:
-                print("El walk esta bien")
+            if tokens[i+1] is not "(":
+                error = True
+                print("(")
+                print(tokens[i] + tokens[i+1] + str(tokens[i+2]) + tokens[i+3] + tokens[i+4])
+                i += 1
             else:
-                print("Error")            
-    return None
+                try:
+                    p = int(tokens[i+2])
+                except:
+                    p = None
+                if p is None:
+                    error = True
+                    print("numero") 
+                    print(tokens[i] + tokens[i+1] + str(tokens[i+2]) + tokens[i+3] + tokens[i+4])
+                    i += 1  
+                else:                                
+                    if tokens[i+3] is not ")":
+                        if  tokens[i+3] not in direcciones or tokens[i+3] not in cardinales:
+                            error = True
+                            print("direccion")
+                            print(tokens[i] + tokens[i+1] + str(tokens[i+2]) + tokens[i+3] + tokens[i+4])
+                            i += 1
+                        if tokens[i+4] is not ")":
+                            error = True
+                            print(")")
+                            print(tokens[i] + tokens[i+1] + str(tokens[i+2]) + tokens[i+3] + tokens[i+4])
+                            i += 1
+                        else:
+                            i += 5  # Avanzar al siguiente conjunto de tokens
+                    else:
+                        i += 5  # Avanzar al siguiente conjunto de tokens
+        else:
+            i += 1  # Avanzar al siguiente token si no es "walk"
+                
+
+
+
+
 
 def checkjump (tokens):
     l = len(tokens)
     for i in range(0, l):
         if tokens[i] == "jump":
             try:
-                p = int(tokens[i+1])
-                q = int(tokens[i+2])
+                p = int(tokens[i+2])
+                q = int(tokens[i+3])
             except:
                 p = None
                 q = None
             if (p is not None) and (q is not None):
                 print("El jump está bien")
             else:
-                print("Error")            
+                print("Error")  
+            if tokens[i+1] is not ("("):
+                print("Falta '('")
+            if tokens[i+4] is not (")"):
+                print("Falta ')'")
+
+
     return None
 
 
@@ -91,3 +129,4 @@ def checkleap (tokens):
 
 
 check_defvar(tokens)
+checkjump(tokens)
